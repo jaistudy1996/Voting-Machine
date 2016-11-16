9af593a54ddc055b3506bc3475a03888fcf7526e
Jayant Arora <jaistudy1996@gmail.com>
2016-11-16 18:47:25 -0500

    Basic setup with crusher.py

--- dict.py
+++ dict.py
@@ -1,8 +1,28 @@
+#! /usr/bin/env python3
+
 '''Data strucutre for the reliable systems project'''
+
+import crusher
 
 class Dict:
 
 	def __init__(self, db, name):
 		'''DB here belongs to class crusher. Name here is the name of the dictionary used.'''
 		self.db = db
-		self.name = name
+		self.internalDict = name
+
+	def insert(self, key, value):
+		self.db.store(key, value)
+
+	def select(self, key):
+		return self.db.fetch(key)
+
+
+if __name__ == "__main__":
+	db = crusher.Broker("testDict.txt")
+
+	test = Dict(db, "test")
+	key = "key1"
+	value = "value1"
+	test.insert(key, value)
+	print(test.select(key))
