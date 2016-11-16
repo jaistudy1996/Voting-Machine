c9a8534f435a80068c122b6caef4e773ecd29c35
Jayant Arora <jaistudy1996@gmail.com>
2016-11-16 18:17:13 -0500

    add __init__ to dict data structure

---
+++ dict.py
@@ -0,0 +1,8 @@
+'''Data strucutre for the reliable systems project'''
+
+class Dict:
+
+	def __init__(self, db, name):
+		'''DB here belongs to class crusher. Name here is the name of the dictionary used.'''
+		self.db = db
+		self.name = name
