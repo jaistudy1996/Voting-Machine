8d02f22973791d5184327ea3e1b4b84d0aa907a4
Jayant Arora <jaistudy1996@gmail.com>
2016-11-22 21:06:57 -0500

    Add function definitions and add file iteration

--- Voting.py
+++ Voting.py
@@ -3,10 +3,36 @@ import crusher
 import dict
 import sys
 
+COMMANDS ={}
 
+def CONF():
+	print("CONF func")
+	pass
+COMMANDS["CONF"] = CONF
+
+def VOTE():
+	print("VOTE func")
+	pass
+COMMANDS["VOTE"] = VOTE
+
+def VOTER():
+	print("VOTER func")
+	pass
+COMMANDS["VOTER"] = VOTER
+
+def CAST():
+	print("CAST func")
+	pass
+COMMANDS["CAST"] = CAST
+
+## File read implementation
 try:
-	file = sys.argv[1]
+	fileName = sys.argv[1]
 except:
-	file = "testDict.txt"
+	fileName = "bigeasy.txt"
 
+file = open(fileName, "r")
 
+for line in file:
+	#print(line.split()[0])
+	COMMANDS[line.split()[0]]()
